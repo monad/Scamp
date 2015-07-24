b5fb026f9aec728efe82d2826e3e14165043c6fa
l-a-i-n <gogdamn@outlook.com>
2015-07-24 18:27:18 +0100

    Fixed indentation up. Thanks Emacs.

--- mpd/client.scm
+++ mpd/client.scm
@@ -2,12 +2,12 @@
   #:version (0 1 0)
   #:use-module (ice-9 rdelim)
   #:use-module (rnrs io ports) 
-	#:use-module (srfi srfi-1)
-	#:use-module (ice-9 regex)
+  #:use-module (srfi srfi-1)
+  #:use-module (ice-9 regex)
   #:export (with-mpd
-						mpd-pause
-						mpd-play
-						mpd-trackinfo))
+            mpd-pause
+            mpd-play
+            mpd-trackinfo))
 
 (define (with-mpd action)
   (let ((sock (socket PF_INET SOCK_STREAM 0)))
@@ -19,29 +19,29 @@
   (write-line "pause" port))
 
 (define (mpd-play port)
-	(write-line "play" port))
+  (write-line "play" port))
 
 (define (mpd-trackinfo port)
   (write-line "currentsong" port)
-	(while #t
-				 (when (char-ready? port)
-							 (break (mpd-read port)))))
+  (while #t
+         (when (char-ready? port)
+               (break (mpd-read port)))))
 
 ;; helpers
 (define (mpd-read! port collection)
   (let ((line (read-line port)))
-		(cond
-		 ((string-ci=? "OK" line)
-			(string-join (reverse collection) "\n"))
-		 ((string-match "ACK(.*)" line) (mpd-error line))
-		 (else (mpd-read! port (cons line collection))))))
+    (cond
+     ((string-ci=? "OK" line)
+      (string-join (reverse collection) "\n"))
+     ((string-match "ACK(.*)" line) (mpd-error line))
+     (else (mpd-read! port (cons line collection))))))
 
 (define (mpd-read port) (mpd-read! port '()))
 
 ;; Processes an error message from MPD.
 (define (mpd-error line)
-	(let* ((data (string-match "ACK(.*)" line))
-				 (pos  (vector-ref data 2)))
-		(if (eq? data #f)
-				"Error: Unknown error."
-				(string-append "Error:" (substring line (car pos) (cdr pos))))))
+  (let* ((data (string-match "ACK(.*)" line))
+         (pos  (vector-ref data 2)))
+    (if (eq? data #f)
+        "Error: Unknown error."
+        (string-append "Error:" (substring line (car pos) (cdr pos))))))
